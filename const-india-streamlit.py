645b9177fbc658169c7fd14f53551660cf282e14
Lakshmi Narayanan <telnarayanan@gmail.com>
2023-02-07 19:37:17 +0530

    Create a new file

---
+++ const-india-streamlit.py
@@ -0,0 +1,27 @@
+# Include Imports Here
+import os
+import numexpr
+import streamlit as st
+from pdfminer.high_level import extract_text
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores.faiss import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI, VectorDBQA
+
+
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+with open('COI_English.txt') as f:
+    sebitext = f.read()
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_text(sebitext)
+
+embeddings = OpenAIEmbeddings()
+docsearch = FAISS.from_texts(texts, embeddings)
+
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
+
+query = "What are the circular numbers in this file?"
+st.write(qa.run(query))
